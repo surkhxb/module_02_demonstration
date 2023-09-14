42ab2b23f3414af4f3284295ef2aae47fdaa051b
surkhab singh sandhu <surkhab24102003sandhu>
2023-09-14 15:15:57 -0500

    CALCULATING

---
+++ module_02_demonstration/README.md
@@ -0,0 +1,11 @@
+# Module 02 Demonstration
+
+## Description
+
+Introduction to Python:  Documentation, Variables, Printing
+
+
+## Author
+
+Student Name
+ 
---
+++ module_02_demonstration/module_02.py
@@ -0,0 +1,11 @@
+"""
+
+Description: Module 02 demonstration
+
+Author: student name
+
+Date: current date
+
+Usage: To demonstrate content from Module 02.
+
+"""
